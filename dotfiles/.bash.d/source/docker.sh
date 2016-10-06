a3010a14aa660402ce7d090086aba978c4039428
ytakagi <ytakagi.deb@gmail.com>
2016-10-06 12:50:28 +0900

    Add alias to use docker for command

---
+++ dotfiles/.bash.d/source/docker.sh
@@ -0,0 +1,26 @@
+if ! which docker > /dev/null; then
+    return;
+fi
+
+ruby_cmds=("ruby")
+node_cmds=("node" "npm")
+
+declare -A imageCmdsMap
+imageCmdsMap=( \
+    ["ytakagi/bundler:2.3-slim"]=${ruby_cmds[@]} \
+    ["node:6.4.0-slim"]=${node_cmds[@]} \
+)
+
+local_images=(`docker images | awk 'NR > 1 {print $1 ":" $2}'`)
+
+for image in ${!imageCmdsMap[@]}; do
+    if [[ " ${local_images[*]} " == *" $image "* ]]; then
+        cmds=${imageCmdsMap[$image][@]}
+        for cmd in ${cmds[@]}; do
+            alias $cmd="docker run -it --rm -w /app -v \$(pwd):/app -v \$HOME:/root/ $image $cmd"
+        done
+        unset cmd
+    fi
+    unset cmds
+done
+unset image
