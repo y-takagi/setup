a386c2d24a087397d8e1403f59c0da1d24c99ac0
y-takagi <ytakagi.deb@gmail.com>
2017-01-06 08:44:32 +0900

    Update setup script
    - Add gitignore_global
    - Remove cask
    - Update peco version

--- setup.sh
+++ setup.sh
@@ -16,14 +16,12 @@ setup_ssh() {
 }
 
 setup_emacs() {
-    git clone https://github.com/cask/cask.git ~/.cask
     git clone https://github.com/y-takagi/emacs.d.git ~/.emacs.d
-    cd ~/.emacs.d && cask install
 }
 
 setup_peco() {
     mkdir ~/tmp && cd ~/tmp/
-    wget "https://github.com/peco/peco/releases/download/v0.3.6/peco_linux_amd64.tar.gz" | tar zxvf
+    wget "https://github.com/peco/peco/releases/download/v0.4.7/peco_linux_amd64.tar.gz" | tar zxvf
     sudo cp peco_linux_amd64/peco /usr/local/bin/
     rm -rf peco_linux_amd64 peco_linux_amd64.tar.gz
 }
